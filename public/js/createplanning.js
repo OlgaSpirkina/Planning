210331a1bf9be2dfccbffc13b55f518d8b1c60bc
Olga <>
2022-05-31 18:09:40 +0200

    work on trainer's planning; create time dropdown

--- public/js/createplanning.js
+++ public/js/createplanning.js
@@ -1,29 +1,32 @@
 window.addEventListener('load', function () {
-  const time = document.getElementById('time');
-  const date = document.getElementById('date');
   const everyClass = document.querySelectorAll('.every-class');
   const totalClasses = document.getElementById('total_classes');
   let countClasses = 0;
-  date.addEventListener('change', function(){
-    if(date.value !== undefined){
-      time.value = date.value + " " + "HH:MM";
-      document.getElementById('label-time').innerHTML = "Remplacez HH par heure et MM par minutes";
-    }
-  });
-  time.addEventListener('change', function(){
-    time.value = time.value + ":00";
-    document.getElementById('label-time').innerHTML = "Heurs du début des cours:";
-  });
-
+  let prevVal = 0;
   for(let i=0; i<everyClass.length; i++){
-    everyClass[i].addEventListener('focusout', e => {
-      if(everyClass[i].value === ''){
-        everyClass[i].value = 0;
+    if(everyClass[i].value === '')everyClass[i].value = 0;
+    /*
+    countClasses += parseInt(everyClass[i].value, 10);
+    everyClass[i].addEventListener('keydown', function (event) {
+      prevVal = parseInt(everyClass[i].value, 10);
+      if (event.keyCode === 8) {
+        countClasses -= prevVal;
+        prevVal = 0;
       }
+      if (event.keyCode === 46) {
+        console.log(prevVal)
+        countClasses -= prevVal;
+        prevVal = 0;
+      }
+      if(everyClass[i].value !== ''){
         countClasses += parseInt(everyClass[i].value, 10);
-        totalClasses.value = countClasses;
-    })
+      }
+      totalClasses.value = countClasses;
+    });
+    */
   }
+
+
   // Alert appears when submit new planning
   document.getElementById('submit-new-class').addEventListener('click', function(){
       (document.getElementById('add-planning').innerHTML ="Créez des nouveaux horaires")
